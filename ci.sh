aa0d905a29fb6e9485e7f7c61c86eb663ae281f2
Cintia Del Rio Calvo <cintiadrio@gmail.com>
2020-09-27 20:59:39 +1000

    Changing CI

--- ci.sh
+++ ci.sh
@@ -2,4 +2,9 @@
 
 set -eu
 
+
+wget https://github.com/UKHomeOffice/repo-security-scanner/releases/download/0.4.0/scanrepo-0.4.0-linux-386.tar.gz
+
+tar xzf scanrepo-0.4.0-linux-386.tar.gz
+
 npm test
