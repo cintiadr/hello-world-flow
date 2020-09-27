ce0cb6c9727f00ecbaca156aecd4b898c72c7f74
Cintia Del Rio Calvo <cintiadrio@gmail.com>
2020-09-27 21:38:01 +1000

    Testing detect

--- ci.sh
+++ ci.sh
@@ -5,6 +5,7 @@ set -eux
 #### Searching for secrets
 SCAN_VERSION="6.1.2"
 wget https://github.com/zricethezav/gitleaks/releases/download/v${SCAN_VERSION}/gitleaks-linux-amd64
+chmod a+x ./gitleaks-linux-amd64
 ./gitleaks-linux-amd64
 
 #### Running unit tests
